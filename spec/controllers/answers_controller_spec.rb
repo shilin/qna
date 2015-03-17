0956ef7943de7002fe8b6dfd334652a38513b9fc
Alexei Shilin <alexei.shilin@gmail.com>
2015-03-17 20:14:13 +0600

    passes that answer belongs to the question it answers

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -10,6 +10,12 @@ RSpec.describe AnswersController, type: :controller do
       it 'saves an answer into db' do
         expect {post :create, answer: attributes_for(:answer), question_id: question}.to change(Answer, :count).by(1)
       end
+
+      it 'saves answer that belongs to the question' do
+        post :create, answer: attributes_for(:answer), question_id: question
+        expect(question.answers.first).to eq assigns(:answer)
+
+      end
       it 'redirects to show' do
         post :create, answer: attributes_for(:answer), question_id: question
         expect(response).to redirect_to [question, assigns(:answer)]
