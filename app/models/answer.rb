cbb5d9f6b9e0de6dd28a09bb48ead393487d2b1d
Alexei Shilin <alexei.shilin@gmail.com>
2015-04-15 21:56:29 +0600

    better order by two fields

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -6,7 +6,7 @@ class Answer < ActiveRecord::Base
 
   before_save :ensure_no_best_answers_left, if: ->() { best_changed? }
 
-  scope :best_first, lambda { order('best DESC') }
+  scope :best_first, -> { order(best: :desc, created_at: :asc) }
 
   protected
 
